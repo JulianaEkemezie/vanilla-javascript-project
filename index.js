e8c4debb8dd05d97050a3f4a2f9024285411c406
juliana <63176299+juli4ict@users.noreply.github.com>
2020-11-28 23:54:50 +0800

    added date and time

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-let now = get.Date()
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
+let now = new Date()
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let day = days[now.getDay()]
+let hour = now.getHours()
+if (hour < 10) {
+    `0${hour}`
+}
+let min = now.getMinutes()
+if (min < 10) {
+    `0${min}`
+}
+let date = document.querySelector("#date")
+date.innerHTML = `Last Updated: ${day} ${hour}:${min}`
