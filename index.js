d169eb009bccbf1a7c5e76e0e133fe04be26d417
juliana <63176299+juli4ict@users.noreply.github.com>
2020-11-29 00:27:06 +0800

    created a functional form

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ if (min < 10) {
 }
 let date = document.querySelector("#date")
 date.innerHTML = `Last Updated: ${day} ${hour}:${min}`
+
+//select form
+// add event
+//select type area
+//set value
+//select display area
+// change innerhtml to value set
+
+function formFunc(event) {
+    event.preventDefault()
+    let cityInput = document.querySelector("#city-input").value
+    let displayCity = document.querySelector("#lagos")
+    displayCity.innerHTML = cityInput
+}
+let form = document.querySelector("#form-submit")
+form.addEventListener("submit", formFunc)
